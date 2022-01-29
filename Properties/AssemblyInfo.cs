1bff327455f7d14ff8cb2ff01a7d17ca557ae3fa
Tryharder <77297444+Tryharderxd@users.noreply.github.com>
2022-01-29 14:22:10 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("2.0.8045.40782")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Lithium 2021")]
+[assembly: AssemblyProduct("LithiumNukerV2")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyFileVersion("2.0.*.*")]
+[assembly: Guid("62bb3eba-f95e-4362-ba1d-72dac7d8b02d")]
+[assembly: AssemblyTitle("LithiumNukerV2")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("Discord nuker by verlox & russian heavy")]
